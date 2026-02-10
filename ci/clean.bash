d07b273a10ccd60b5e9516cdb002b824171fe4d3
Xaabibas <tumuyhabirov@yandex.ru>
2026-02-10 11:50:26 +0300

    Initial commit

---
+++ ci/clean.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+
+cd "$(dirname "$0")"
+
+OUT=out
+
+rm -rf ../$OUT
---
+++ contest/sample/b/Main.cpp
@@ -0,0 +1,8 @@
+#include <cassert>
+#include <iostream>
+
+int main() {
+  int number = 0;
+  std::cin >> number;
+  assert(number == 1);
+}
