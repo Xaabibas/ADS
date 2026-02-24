211701ed20323e766a426c9b98dce87f4d718596
Xaabibas <tumuyhabirov@yandex.ru>
2026-02-24 20:33:01 +0000

    f done

--- contest/f/Main.cpp
+++ contest/f/Main.cpp
@@ -4,29 +4,29 @@
 #include <vector>
 
 bool Compare(const std::string& string1, const std::string& string2) {
-  int k = std::min(string1.size(), string2.size());
+  std::string string12;
+  string12.append(string1).append(string2);
+  std::string string21;
+  string21.append(string2).append(string1);
 
-  for (int i = 0; i < k; i++) {
-    if (string1[i] > string2[i]) {
-      return true;
-    }
-    if (string2[i] > string1[i]) {
-      return false;
-    }
-  }
-
-  return string1.size() > string2.size();
+  return string12 > string21;
 }
 
 int main() {
-  std::pmr::vector<std::string> data;
+  std::vector<std::string> data;
   std::string line;
 
   while (std::getline(std::cin, line)) {
     if (line == "exit") {
       break;
     }
-    data.push_back(line);
+    if (!line.empty()) {
+      data.push_back(line);
+    }
+  }
+
+  for (std::string& str : data) {
+    std::cout << str << "\n";
   }
 
   std::sort(begin(data), end(data), Compare);
