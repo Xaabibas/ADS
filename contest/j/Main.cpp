b70bf2c0fa642d1b6f35db6cff3c266bc57d5d49
Xaabibas <tumuyhabirov@yandex.ru>
2026-04-02 15:09:39 +0000

    task J

---
+++ contest/j/Main.cpp
@@ -0,0 +1,73 @@
+#include <deque>
+#include <iostream>
+#include <string>
+struct Node {
+  int value;
+  Node* next;
+  Node* prev;
+};
+
+class CustomQueue {
+private:
+  std::deque<int> head_;
+  std::deque<int> tail_;
+
+  void Balance() {
+    int temp = tail_.front();
+    tail_.pop_front();
+    head_.push_back(temp);
+  }
+
+public:
+  void Pop() {
+    head_.pop_front();
+    if (head_.size() < tail_.size()) {
+      Balance();
+    }
+  }
+
+  [[nodiscard]] int Top() const {
+    return head_.front();
+  }
+
+  void PushBack(int value) {
+    tail_.push_back(value);
+
+    if (tail_.size() > head_.size()) {
+      Balance();
+    }
+  }
+
+  void PushCenter(int value) {
+    if (head_.size() > tail_.size()) {
+      tail_.push_front(value);
+    } else {
+      head_.push_back(value);
+    }
+  }
+};
+
+int main() {
+  int n = 0;
+  std::cin >> n;
+  std::string sym;
+  CustomQueue queue;
+
+  for (int i = 0; i < n; i++) {
+    std::cin >> sym;
+
+    if (sym == "+") {
+      int value = 0;
+      std::cin >> value;
+      queue.PushBack(value);
+    } else if (sym == "*") {
+      int value = 0;
+      std::cin >> value;
+      queue.PushCenter(value);
+    } else {
+      int top = queue.Top();
+      queue.Pop();
+      std::cout << top << "\n";
+    }
+  }
+}
